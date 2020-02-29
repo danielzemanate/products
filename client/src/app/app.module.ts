638cf1ec78c342dfd7329c23ad99c0157a0eb804
danielzemanate <danielzemanate@gmail.com>
2020-02-29 13:19:23 -0500

    CLIENT

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router'
+
+import { AppComponent } from './app.component'
+import { ProfileComponent } from './profile/profile.component'
+import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component'
+import { HomeComponent } from './home/home.component'
+import { AuthenticationService } from './authentication.service'
+import { AuthGuardService } from './auth-guard.service';
+import { ProductsFormComponent } from './products-form/products-form.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import {ProductosService} from './services/productos.service'
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuardService]
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProductsFormComponent,
+    ProductsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [AuthenticationService, 
+              AuthGuardService,
+              ProductosService
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
